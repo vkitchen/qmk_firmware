934ea3eb88c9edecdb31471e7d7537e8523ba866
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2022-07-02 16:37:56 +1200

    Reprogram as APTv3

--- keyboards/keebio/iris/keymaps/vkitchen/keymap.c
+++ keyboards/keebio/iris/keymaps/vkitchen/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 
-#define _QWERTY 0
+#define _APT 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
@@ -15,15 +15,15 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+  [_APT] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+     KC_TAB,  KC_W,    KC_G,    KC_D,    KC_F,    KC_B,                               KC_Q,    KC_L,    KC_U,    KC_O,    KC_Y,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LCTL, KC_R,    KC_S,    KC_T,    KC_H,    KC_K,                               KC_J,    KC_N,    KC_E,    KC_A,    KC_I,    KC_SCLN,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_X,    KC_C,    KC_M,    KC_P,    KC_V,    KC_HOME,          KC_END,  KC_Z,    KC_COMM, KC_DOT,  KC_QUOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -76,7 +76,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_APT);
       }
       return false;
       break;
