4b59b2add187a3ac4fdcda07c5555cbdd1c3ec2c
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-03-27 17:31:38 +1300

    Redefine layers

--- keyboards/keebio/iris/keymaps/vkitchen/keymap.c
+++ keyboards/keebio/iris/keymaps/vkitchen/keymap.c
@@ -37,35 +37,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL, KC_R,    KC_S,    KC_T,    KC_H,    KC_K,                               KC_J,    KC_N,    KC_E,    KC_A,    KC_I,    KC_SCLN,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_X,    KC_C,    KC_M,    KC_P,    KC_V,    KC_HOME,          KC_END,  KC_Z,    KC_COMM, KC_DOT,  KC_QUOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_X,    KC_C,    KC_M,    KC_P,    KC_V,    KC_HOME,          KC_END,  KC_Z,    KC_COMM, KC_DOT,  KC_QUOT, KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
+                                    KC_LCMD, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWERED] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+     KC_F11,  KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,                              KC_F10,  KC_F2,   KC_F4,   KC_F6,   KC_F8,   KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, KC_MINS, KC_AT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL,  _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_PIPE,
+     _______, _______, _______, _______, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_DLR,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, _______, _______, _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_BSLS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                    KC_DEL,  _______, KC_P0
+                                    _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_RAISED] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     KC_F11,  KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,                              KC_F10,  KC_F2,   KC_F4,   KC_F6,   KC_F8,   KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSLS,
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -85,6 +85,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
+
+void unregister_shift(void) {
+  if ((get_mods() & MOD_BIT(KC_LSFT)) == MOD_BIT(KC_LSFT)) {
+    unregister_code(KC_LSFT);
+  } else if ((get_mods() & MOD_BIT(KC_RSFT)) == MOD_BIT(KC_RSFT)) {
+    unregister_code(KC_RSFT);
+  }
+}
+
+void register_shift(void) {
+  if ((get_mods() & MOD_BIT(KC_LSFT)) == MOD_BIT(KC_LSFT)) {
+    register_code(KC_LSFT);
+  } else if ((get_mods() & MOD_BIT(KC_RSFT)) == MOD_BIT(KC_RSFT)) {
+    register_code(KC_RSFT);
+  }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -134,15 +150,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   /* Doesn't support key repetion or both shifts simultaneously */
   if (layer_state_is(_SHIFTED) && record->event.pressed) {
     if ((KC_1 <= keycode && keycode <= KC_0) || keycode == KC_GRV) {
-      if ((get_mods() & MOD_BIT(KC_LSHIFT)) == MOD_BIT(KC_LSFT)) {
-        unregister_code(KC_LSFT);
-        tap_code(keycode);
-        register_code(KC_LSFT);
-      } else {
-        unregister_code(KC_RSFT);
-        tap_code(keycode);
-        register_code(KC_RSFT);
-      }
+      unregister_shift();
+      tap_code(keycode);
+      register_shift();
+      return false;
+    }
+    if (keycode == KC_AT) {
+      tap_code(KC_6); /* We want caret */
+      return false;
+    }
+    if (keycode == KC_DLR) {
+      tap_code(KC_GRV); /* We want tilde */
       return false;
     }
   }
